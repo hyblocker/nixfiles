1dee9c2363dbc4069f402c2d65eca5ebc9b8a292
Hyblocker <hyblocker@protonmail.com>
2025-10-20 02:08:44 +0200

    (fix): Invalid time format

--- common/base.nix
+++ common/base.nix
@@ -68,7 +68,7 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 1w";
+    options = "--delete-older-than 7d";
   };
   # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
   nix.settings.auto-optimise-store = true;
