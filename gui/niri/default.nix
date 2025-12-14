cba850c19d63fd7ff1966f8095ef6beea6b68acd
Hyblocker <hyblocker@protonmail.com>
2025-12-14 15:04:49 +0100

    fix: try fixing keyring not auto-unlocking with sddm

--- gui/niri/default.nix
+++ gui/niri/default.nix
@@ -30,6 +30,7 @@
 
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.login.enableGnomeKeyring = true;
+  security.pam.services.sddm.enableGnomeKeyring = true;
 
   imports = [
     ./noctalia.nix
