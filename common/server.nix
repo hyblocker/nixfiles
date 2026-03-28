a08546331c27f1a4bb0ae700b15699d84ccacc2c
Hyblocker <hyblocker@protonmail.com>
2026-03-28 14:06:56 +0100

    fix: deprecation

--- common/server.nix
+++ common/server.nix
@@ -22,12 +22,12 @@ in
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
 
-  systemd.sleep.extraConfig = ''
-    AllowSuspend=no
-    AllowHibernation=no
-    AllowHybridSleep=no
-    AllowSuspendThenHibernate=no
-  '';
+  systemd.sleep.settings.Sleep = {
+    AllowSuspend = "no";
+    AllowHibernation = "no";
+    AllowHybridSleep = "no";
+    AllowSuspendThenHibernate = "no";
+  };
 
   # enable nginx reverse proxy
   services.nginx = {
