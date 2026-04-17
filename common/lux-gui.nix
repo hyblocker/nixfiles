4e3f951a7e1d9c229e827ce5dd25ef7ad2f8d3ea
Hyblocker <hyblocker@protonmail.com>
2026-04-17 15:05:00 +0200

    fix: unfuck audio for non-sdvx apps

--- common/lux-gui.nix
+++ common/lux-gui.nix
@@ -36,16 +36,11 @@
     extraConfig.pipewire."92-samplerates" = {
       "context.properties" = {
         "default.clock.rate" = 44100;
-        "default.clock.allowed-rates" = [ 44100 ];
-      };
-    };
-    # low latency
-    extraConfig.pipewire."92-low-latency" = {
-      "context.properties" = {
-        "default.clock.rate" = 44100;
-        "default.clock.quantum" = 32;
-        "default.clock.min-quantum" = 32;
-        "default.clock.max-quantum" = 32;
+        "default.clock.allowed-rates" = [
+          44100
+          48000
+          96000
+        ];
       };
     };
 
